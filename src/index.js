aa908b8e2bb59f977130157e181a3c1183e0a8d6
Nityasha Malik <nityasha.malik.ug22@nsut.ac.in>
2025-07-09 21:23:53 +0530

    Fix: remove unused Test component to fix Netlify build

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ import App from "./App";
 import StarRating from "./StarRating";
 import { useState } from "react";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="green" maxRating={10} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars.</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="green" maxRating={10} onSetRating={setMovieRating} />
+//       <p>This movie was rated {movieRating} stars.</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
